16da865bbbbef460a853432fe042e0dea92375b0
jp <jjorissen52@gmail.com>
2017-12-06 17:35:02 -0600

    restructuring for themes

--- golf_site2/settings.py
+++ golf_site2/settings.py
@@ -1,4 +1,5 @@
 import os, configparser, socket
+from django.utils.translation import ugettext_lazy as _
 gettext = lambda s: s
 DATA_DIR = os.path.dirname(os.path.dirname(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -17,62 +18,28 @@ TIME_ZONE = 'America/Chicago'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_URL = '/static/'
-# MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'golf_site2', 'media')
-# STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'golf_site2', 'static'),
-    os.path.join(BASE_DIR, 'golf_site2', 'binary_assets'),
+    os.path.join(BASE_DIR, 'golf_site2', 'themes', 'binary', 'static'),
+    os.path.join(BASE_DIR, 'golf_site2', 'themes', 'binary', 'assets'),
     os.path.join(BASE_DIR, 'events', 'static'),
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'node_modules'),
 )
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = config.get('golf_site2', 'storage_bucket_name')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# STATIC_URL = "https://%s/static/" % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = '/static/'
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 MEDIA_URL = "https://%s/media/" % AWS_S3_CUSTOM_DOMAIN
 MEDIAFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE = 'short_term_reports.custom_storages.StaticStorage'
-
-
-SITE_ID = 1
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'golf_site2', 'binary_templates'), ],
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.template.context_processors.media',
-                'django.template.context_processors.csrf',
-                'django.template.context_processors.tz',
-                'sekizai.context_processors.sekizai',
-                'django.template.context_processors.static',
-                'cms.context_processors.cms_settings'
-            ],
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-                'django.template.loaders.eggs.Loader'
-            ],
-        },
-    },
-]
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MIDDLEWARE_CLASSES = (
     'cms.middleware.utils.ApphookReloadMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -92,7 +59,6 @@ INSTALLED_APPS = (
     'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'django.contrib.messages',
-    'cms',
     'menus',
     'sekizai',
     'treebeard',
@@ -105,6 +71,8 @@ INSTALLED_APPS = (
     'cmsplugin_filer_folder',
     'cmsplugin_filer_image',
     'cmsplugin_filer_utils',
+    'cmsplugin_cascade',
+    'cms',
     'djangocms_style',
     'djangocms_snippet',
     'djangocms_googlemap',
@@ -136,6 +104,36 @@ CMS_LANGUAGES = {
     },
 }
 
+SITE_ID = 1
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'golf_site2', 'themes', 'binary', 'templates'),
+            # os.path.join(BASE_DIR, 'golf_site2', 'themes', 'base', 'templates'),
+         ],
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.template.context_processors.media',
+                'django.template.context_processors.csrf',
+                'django.template.context_processors.tz',
+                'sekizai.context_processors.sekizai',
+                'django.template.context_processors.static',
+                'cms.context_processors.cms_settings'
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+                'django.template.loaders.eggs.Loader'
+            ],
+        },
+    },
+]
 CMS_TEMPLATES = (
     ## Customize this
     ('fullwidth.html', 'Fullwidth'),
@@ -145,16 +143,44 @@ CMS_TEMPLATES = (
 )
 
 CMS_PERMISSION = True
+CMSPLUGIN_CASCADE_PLUGINS = ['cmsplugin_cascade.bootstrap3']
+CMSPLUGIN_CASCADE_PLUGINS.append('cmsplugin_cascade.generic')
+CMSPLUGIN_CASCADE_PLUGINS.append('cmsplugin_cascade.icon')
 
-CMS_PLACEHOLDER_CONF = {}
-
-
+CMSPLUGIN_CASCADE = {
+    'bootstrap3': {
+        'breakpoints': (
+            ('xs', (768, 'mobile-phone', _("mobile phones"), 750, 768)),
+            ('sm', (768, 'tablet', _("tablets"), 750, 992)),
+            ('md', (992, 'laptop', _("laptops"), 970, 1200)),
+            ('lg', (1200, 'desktop', _("large desktops"), 1170, 2500)),
+            # ('xl', (1600, 'desktop icon-large', _("xlarge desktops"), 1570, 8000)),
+        ),
+    }
+}
+CMS_PLACEHOLDER_CONF = {
+    'Main Content Placeholder': {
+        'plugins': ['BootstrapContainerPlugin'],
+        'text_only_plugins': ['TextLinkPlugin'],
+        'parent_classes': {'BootstrapContainerPlugin': None},
+        'glossary': {
+            'breakpoints': ['xs', 'sm', 'md', 'lg'],
+            'container_max_widths': {'xs': 750, 'sm': 750, 'md': 970, 'lg': 1170},
+            'fluid': False,
+            'media_queries': {
+                'xs': ['(max-width: 768px)'],
+                'sm': ['(min-width: 768px)', '(max-width: 992px)'],
+                'md': ['(min-width: 992px)', '(max-width: 1200px)'],
+                # 'lg': ['(min-width: 1200px)'],
+            },
+        },
+    },
+}
 if config.get('local', 'host_name') in socket.gethostname():
     DATABASES = {
         'default': {
-            'ENGINE': 'zappa_django_utils.db.backends.s3sqlite',
+            'ENGINE': 'django.db.backends.sqlite3',
             'NAME': 'golf_site2.db',
-            'BUCKET': config.get('lambda', 'db_bucket_name')
         }
     }
 else:
@@ -174,14 +200,6 @@ else:
         }
     }
 
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'golf_site2.db',
-#     }
-# }
 
 
 MIGRATION_MODULES = {
